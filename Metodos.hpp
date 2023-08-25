86e10272370cdd978f955dac76a5f92455aa7e95
Jose Andres <joseasalazar04@gmail.com>
2023-08-25 12:25:27 -0600

    se actualiza

---
+++ Metodos.hpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+//Lista doble enlazada
+
+void Buscar(string nombre){
+/*Entra el nombre y seria pasar por la lista y sacar/imprimir los que tengan lo que ingreso el usuario en el titulo y se le
+muestra las que coinciden 
+*/
+
+}
+
+void AgregarNoticia(){
+/*Ingresar una noticia y simplemente meterla a la lista 
+
+*/
+}
+
+
+void IngresarEspecifico(string nombre, int posi){
+/*Este es para cuando uno quiere cambiar la relevancia. Para que se ingrese en una posicion que desea el usuario
+*/
+}
+
+void Eliminar(string nombre){
+/*Se ingresa el nombre de la noticia y la elimina
+*/
+
+}
+
+
+int Largo(){
+/*Esto me devolveria el largo de la lista que se usa
+*/
+
+}
+
+void Mostrar(){
+/*Me mostraria toda la lista con el orden de relevancia, y seria mas o menos asi
+NombreNoticia - Visitas 
+*/
+
+}
+
+void getNext(){
+//Para la lista
+
+
+
+}
+
+
+
+
+
+
+
---
+++ noticias.hpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+void EliminarTotal(string nombre){
+/*
+Buscar(string nombre)
+Se pregunta una confirmacion
+Eliminar(string nombre)
+*/
+}
+
+
+
+void Top5(){
+/*
+Mostrar()
+pero con un for desde fuera para que solo imprima las 5 primeras
+*/
+
+}
+
+void Mostrar(){
+    /*se usaria el metod que esta en el metodos.hpp*/
+}
+
+
+void CambiarRelevancia(string nombre,int cambio){
+/*
+Buscar(nombre)
+Eliminar(nombre)
+IngresarEspecifico(string nombre,int cambio) aqui se usaria el mismo nombre y para el int seria agarrar el que tenia al principio
+y luego haberle sumado o restado el que el usuario ingreso
+*/
+
+}
+
+
+
+
+
